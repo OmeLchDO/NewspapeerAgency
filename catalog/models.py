955db731c5915eac6c2b8a3f3b30f5c3800d8f96
itOmelchDO <it.omelchenko@gmail.com>
2023-03-22 23:47:41 +0200

    'Add first migrations - created database with Topic & Newspaper $ Redactor'

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(models.Model):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="topic",
+    )
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return f"{self.title}"
+
+
