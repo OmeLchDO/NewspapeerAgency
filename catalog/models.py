a6a27472d5219744da6139b0d7f52d401ba82e98
itOmelchDO <it.omelchenko@gmail.com>
2023-03-23 00:13:25 +0200

    'refactor models.py_Added AbstractUser, edited classes Topic, Redactor, Newspaper'

--- catalog/models.py
+++ catalog/models.py
@@ -1,23 +1,23 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
 
-class Redactor(models.Model):
+class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(null=True, blank=True)
-    username = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return f"{self.username} ({self.first_name} {self.last_name})"
+    class Meta:
+        ordering = ["username"]
 
 
 class Newspaper(models.Model):
@@ -29,7 +29,10 @@ class Newspaper(models.Model):
         on_delete=models.CASCADE,
         related_name="topic",
     )
-    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self):
         return f"{self.title}"
