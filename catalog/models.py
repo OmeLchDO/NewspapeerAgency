73f0700069c9b1f8416c45d7144ac21050c2f955
itOmelchDO <it.omelchenko@gmail.com>
2023-03-23 20:10:20 +0200

    'Refactor models.py, add migrations_0003'

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         ordering = ["name"]
@@ -17,7 +17,12 @@ class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(null=True, blank=True)
 
     class Meta:
+        verbose_name = "Publisher"
+        verbose_name_plural = "Publishers"
         ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name}"
 
 
 class Newspaper(models.Model):
