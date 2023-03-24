326eef4d16661f2988b60ab2a55e00c8042284d3
itOmelchDO <it.omelchenko@gmail.com>
2023-03-25 00:13:29 +0200

    ' Create main page'

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Newspaper, Redactor, Topic
+
+
+def index(request):
+    num_newspapers = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+
+    context = {
+        "num_newspapers": num_newspapers,
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+    }
+    return render(request, "catalog/index.html", context=context)
