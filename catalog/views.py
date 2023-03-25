0dc35c34b53247b9a427a7f54a7817201f318359
itOmelchDO <it.omelchenko@gmail.com>
2023-03-26 00:50:30 +0200

    'Create pages in urls.py & views.py, add pages *.html'

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from catalog.models import Newspaper, Redactor, Topic
 
@@ -15,3 +16,21 @@ def index(request):
         "num_topics": num_topics,
     }
     return render(request, "catalog/index.html", context=context)
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+    template_name = "catalog/topic_list.html"
+
+
+class NewspaperListView(generic.ListView):
+    model = Newspaper
+    template_name = "catalog/newspaper_list.html"
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    template_name = "catalog/redactor_list.html"
+
+
+
