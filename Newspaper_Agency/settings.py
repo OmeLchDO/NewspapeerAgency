9af8993f38e98607b95ffd46a36107d53f44d26f
itOmelchDO <it.omelchenko@gmail.com>
2023-04-06 16:53:21 +0300

    'Fixed db with problems migrations'

--- Newspaper_Agency/settings.py
+++ Newspaper_Agency/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = "Newspaper_Agency.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
